80b369988ad928046843922b7701c7a9ed11b81c
TheFlash55 <1779557765@qq.com>
2024-03-20 20:57:54 +0800

    第一模块-项目初始化

---
+++ src/views/Category/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+    我是分类页
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/Home/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+    我是home页
+</template>
+
+<style scoped>
+
+</style>
